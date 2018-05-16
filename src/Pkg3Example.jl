bc9399a6e15a55a4ca4bcb510e0768ef291321bd
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-05-16 13:56:09 +0200

    stuff

---
+++ src/Pkg3Example.jl
@@ -0,0 +1,10 @@
+__precompile__()
+module Pkg3Example
+
+using JSON
+using ImportMacros
+
+greet() = print("Hello World!")
+foo() = 1+1
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using Test
+using Pkg3Example
+
+@test Pkg3Example.foo() == 2
